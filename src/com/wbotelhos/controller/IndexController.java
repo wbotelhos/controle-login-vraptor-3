f74779c649c053044bb74d09489ca0d924baa028
wbotelhos <wbotelhos@gmail.com>
2011-01-01 13:48:11 -0200

    Atualizado comentários.

--- src/com/wbotelhos/controller/IndexController.java
+++ src/com/wbotelhos/controller/IndexController.java
@@ -5,8 +5,8 @@ import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 
 /**
- * @author Washington Botelho dos Santos
- * @artigo http://wbotelhos.com/2010/04/07/controle-de-login-com-vraptor-3
+ * @author Washington Botelho
+ * @artigo http://wbotelhos.com.br/2010/04/07/controle-de-login-com-vraptor-3
  */
 
 @Resource
