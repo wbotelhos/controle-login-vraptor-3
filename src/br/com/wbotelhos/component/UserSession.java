bcc5a3056b904f3c7ae0ddf7e43ffdfb0e35f5bd
Washington Botelho <wbotelhos@gmail.com>
2011-09-17 19:44:38 -0300

    Movido o UserSession para o pacote de componente e adicionado métodos utilitários.

---
+++ src/br/com/wbotelhos/component/UserSession.java
@@ -0,0 +1,34 @@
+package br.com.wbotelhos.component;
+
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.SessionScoped;
+import br.com.wbotelhos.model.Usuario;
+
+/**
+ * @author Washington Botelho
+ * @article http://wbotelhos.com.br/2010/04/07/controle-de-login-com-vraptor-3
+ */
+
+@Component
+@SessionScoped
+public class UserSession {
+
+	private Usuario user;
+
+	public boolean isLogged() {
+		return user != null;
+	}
+
+	public void logout() {
+		user = null;
+	}
+
+	public Usuario getUser() {
+		return user;
+	}
+
+	public void setUser(Usuario user) {
+		this.user = user;
+	}
+
+}
