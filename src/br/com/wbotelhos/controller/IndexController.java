1f5f66a5dd9f0894065a00371c930ae79ae8930d
Washington Botelho <wbotelhos@gmail.com>
2011-09-17 19:45:17 -0300

    Aproveitado nova feature para path.

--- src/br/com/wbotelhos/controller/IndexController.java
+++ src/br/com/wbotelhos/controller/IndexController.java
@@ -1,20 +1,19 @@
 package br.com.wbotelhos.controller;
 
 import br.com.caelum.vraptor.Get;
-import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 
 /**
  * @author Washington Botelho
- * @artigo http://wbotelhos.com.br/2010/04/07/controle-de-login-com-vraptor-3
+ * @article http://wbotelhos.com.br/2010/04/07/controle-de-login-com-vraptor-3
  */
 
 @Resource
 public class IndexController {
 
-	@Get
-	@Path("/")
+	@Get("/")
 	public void index() {
+
 	}
 
 }
